adaaca556e7aaf986609711caed52dab94627fd8
Javier Wamba <javierw81@gmail.com>
2020-12-19 03:04:37 +0100

    style components implementation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Title } from './src/components/Title';
+import { View } from './src/components/View';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Title>Open up App.tsx to start working on your app!</Title>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
